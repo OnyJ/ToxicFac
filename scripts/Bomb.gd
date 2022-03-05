f44c4464532d60a8b5a75507e2242554e5cb15e7
Josselin Julien <julien.josselin75@hotmail.fr>
2022-03-05 18:07:16 +0100

    bomb prototype

---
+++ scripts/Bomb.gd
@@ -0,0 +1,28 @@
+extends Sprite
+
+func _ready():
+	# start timer
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func bomb_is_toxic():
+	# Toxic color
+	# Toxic animation
+	pass
+func bomb_will_explode():
+	# Danger color
+	# Danger animation
+		# create fast blinking animation
+	pass
+func bomb_is_saved():
+	# Success color
+	# Success animation
+	pass
+func bomb_explodes():
+	# Explosion color
+	# Explosion animation
+	pass
