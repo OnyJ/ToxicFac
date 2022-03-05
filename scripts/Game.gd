d3f6fab04534a0ad8c6e974c2979971fb98be08b
Josselin Julien <julien.josselin75@hotmail.fr>
2022-03-05 12:41:15 +0100

    start/stop game MainMenu

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,11 +1,39 @@
 extends Node2D
 
-# variables
+export (PackedScene) var Ui = preload("res://scenes/UI.tscn")
+
+#var ui = preload("res://scenes/UI.tscn").instance()
+
+
+var GameState = "MainMenu"
+
 
 func _ready():
-	print("hello")
+	print(GameState)
+
+
+func _unhandled_input(event): # to Play again
+	# event is good for a one time action
+	if event.is_action_pressed("ui_accept"):
+		start_game()
+	if event.is_action_pressed("ui_cancel"):
+		game_over()
+
+
+func start_game():
+	GameState = "Playing"
+	$UI/MainMenu.hide()
+	print(GameState)
+func game_over():
+	GameState = "MainMenu"
+	$UI/MainMenu.show()
+	print(GameState)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+
